1a2c6c5d9cd2d099a9d2dec27ae6f6153611c1bd
Oleksii Kucheruk <oleksii.kucheruk+github@teamdev.com>
2022-10-05 20:30:11 +0300

    Fix `try` order

--- EntryPoint.ts
+++ EntryPoint.ts
@@ -3,9 +3,9 @@ import NodejsExpressRaccoonizer from './src/NodejsExpressRaccoonizer';
 
 try {
     assert( process.env.PORT, "Checks if PORT environment variable exist" );
-} catch( exception ) {
-    console.log( exception );
-} finally {
     const atPort = Number( process.env.PORT );
     new NodejsExpressRaccoonizer( atPort ).run();
+
+} catch( exception ) {
+    console.log( exception );
 }
