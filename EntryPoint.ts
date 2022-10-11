699e7e9561ff25100ac909821850be105f0c61c5
Oleksii Kucheruk <oleksii.kucheruk+github@teamdev.com>
2022-10-11 15:24:48 +0300

    Update readme

--- EntryPoint.ts
+++ EntryPoint.ts
@@ -5,9 +5,6 @@ try {
     assert( process.env.PORT, "Fails if PORT environment variable do not exist." );
     const atPort = Number( process.env.PORT );
     new KoaMicroservice( atPort ).deploy();
-    //new HapiMicroservice( atPort ).deploy();
-    //new ExpressMicroservice( atPort ).deploy( );
-
 } catch( exception ) {
     console.log( exception );
 }
