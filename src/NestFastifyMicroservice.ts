c3fc41760d057c3ae054eb539a939c7d4a33ccf9
Oleksii Kucheruk <oleksii.kucheruk+github@teamdev.com>
2022-11-08 09:13:18 +0200

    Implement `NestFastifyMicroservice`

---
+++ src/NestFastifyMicroservice.ts
@@ -0,0 +1,36 @@
+import NodejsFramework from "./NodejsFramework";
+import Microservice from "./Microservice";
+import WebAppModule from './WebAppModule';
+import { NestFastifyApplication } from '@nestjs/platform-fastify';
+import { NestFactory } from '@nestjs/core';
+import { FastifyAdapter } from '@nestjs/platform-fastify';
+
+/**
+ * A  Nest Fastify NodeJS microservice and your configuration.
+ */
+export default class NestFastifyMicroservice
+  extends NodejsFramework<Promise<NestFastifyApplication>>
+  implements Microservice {
+
+  private port: number;
+
+  constructor(port: number) {
+    super(
+      NestFactory.create<NestFastifyApplication>(
+        WebAppModule,
+        new FastifyAdapter({ logger: true })
+    ));
+    this.port = port;
+  }
+
+  //@Implementation
+  public deploy(): void {
+    this.instance()
+      .then((app) => {
+        app.listen(
+          this.port,
+          () => { console.log("%s listening on port %s", this.constructor.name, this.port ) }
+        );
+      });
+  }
+}
