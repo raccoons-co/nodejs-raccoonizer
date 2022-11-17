3bc85443745d967b68108ed8dc1eee3c1d531b90
Oleksii Kucheruk <ok+github@raccoons.co>
2022-11-17 10:51:31 +0200

    Fix merge

--- src/test/KoaConfiguration.ts
+++
@@ -1,9 +0,0 @@
-import { NodejsFrameworkConfiguration } from "../main/index";
-import Koa from "koa";
-
-export default class KoaConfiguration implements NodejsFrameworkConfiguration<Koa> {
-  applyTo( koaInstance: Koa ): void {
-    koaInstance
-      .use(async ( ctx: Koa.Context ) => { ctx.body = "Hello World"; } );
-  }
-}
